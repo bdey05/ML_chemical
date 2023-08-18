622ae68cba469529929ccc6af437a3421cf0990f
caitlinr77 <126729801+caitlinr77@users.noreply.github.com>
2023-08-18 17:16:50 -0400

    Update pH_calculator.py

--- pH_calculator.py
+++ pH_calculator.py
@@ -1,41 +1,52 @@
-
 import json
 import numpy as np
 import math 
 
+
 def alpha(file_name):
 
     with open(file_name, "r",  encoding = 'utf-8') as file:
+
     
         json_data = json.load(file)
+
     
     filtered_data = []
+
     
     for record in json_data:
+
         
         filtered_data.append(record["aqueous phase"])
 
-    #this will print a list of associated name-value pairs of all data within the aqueous phase key
-    
-    for lst in filtered_data:
+
+    # prints a list of associated name-value pairs of all data, within the aqueous phase object
+            
+    # accessing pH to see if value already provided
+
+    # if pH is missing, checking provided concentration of the strong acid-nitric acid, for pH calculation
+         
+    pH = filtered_data[1]
+
+
+    acid_conc = filtered_data [0]
+
+
+    if isinstance(pH, (int, float)):
+
+            
+        pH = np.array("pH", dtype=float)
+
+                        
+        return h3o
         
-        pH = filtered_data[1]
-        
-        if isinstance(pH, (int, float)):
-            
-            pH = np.array("pH", dtype=float)
-            
-            h3o = -math.log10(pH)
-            
-            return h3o
-        
-        elif:
-            
-            for lst_2 in filtered_data:
+    elif isinstance(acid_conc, (int, float)):
                 
-                pH = filtered_data [0]
+        acid_conc = np.array("pH", dtype=float)
 
-#trying to access all information within "solutes" in the list containing all aqueous phase data, to access concentration of strong acid and use that for pH
-            
+        h3o = -math.log10(pH)
 
+        return h3o
+
+        
 alpha("all_TPEN_DERIVATIVE_records.json")
