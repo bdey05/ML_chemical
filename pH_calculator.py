1125a4fc9c98a0ef2dee9ef0d5943811bdecca61
caitlinr77 <126729801+caitlinr77@users.noreply.github.com>
2023-08-18 17:28:19 -0400

    Update pH_calculator.py

--- pH_calculator.py
+++ pH_calculator.py
@@ -31,22 +31,47 @@ def alpha(file_name):
 
     acid_conc = filtered_data [0]
 
+    
+    if (isinstance(json_data, list)):
+        
+        for record in json_data:
+                        
+            filtered_data.append(record["aqueous phase"])
+            
 
-    if isinstance(pH, (int, float)):
+            print(record['aqueous phase']['solutes'])
+            
+
+    if (isinstance(json_data, dict)):
+        
+                
+        filtered_data.append(json_data["aqueous phase"])
+        
+        
+        print(json_data["aqueous phase"])
+
+
+
+    for record in filtered_data:
+        
+        if (isinstance(record['pH'], int, float)):
+
+            pH = np.array("pH", dtype=float)
+            
+            print(f"pH is present and valid, pH is {pH}.")
 
             
-        pH = np.array("pH", dtype=float)
+        else:
+            
+            print("pH is not int. Here are the solutes:", record['solutes'])
 
-                        
-        return h3o
-        
-    elif isinstance(acid_conc, (int, float)):
-                
-        acid_conc = np.array("pH", dtype=float)
+            solutes = np.array("pH", dtype=float)
 
-        h3o = -math.log10(pH)
+            h3o = -math.log10(pH)
 
-        return h3o
+            return h3o
+
+
 
         
 alpha("all_TPEN_DERIVATIVE_records.json")
